cfff3fa29136de26d835943ef976906d0f1be86c
versatts <versatts@163.com>
2026-09-25 12:06:04 +0800

    add

---
+++ DesktopAppBox/TabHead.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#include "imgui_internal.h"
+#include <vector>
+#include <string>
+class TabHead
+{
+public:
+	void Render();
+	int* selectedTabIdx = nullptr;
+	std::vector<std::wstring>* folders = nullptr;
+};
+
